af9302459bff5d41a32b31afb4e6f9ca0f431f4d
Yury Kavaliou <developer@EPBYMINW4779.minsk.epam.com>
2015-06-26 17:30:04 +0300

    Completed 32 sensors of current data.

--- obdlib/test.py
+++ obdlib/test.py
@@ -1,8 +1,22 @@
 import scanner
 import time
 
-with scanner.OBDScanner("/dev/pts/7") as scan:
+
+# Retrieves all available sensor values
+with scanner.OBDScanner("/dev/pts/5") as scan:
     while True:
+        data = {}
+        for sensor in scan.sensor.sensors(mode=01):
+            # gets available sensor value only
+            data[sensor.title] = sensor.value
+        print("---------------------------------------")
+        print(data)
         time.sleep(0.5)
-        sensor = scan.sensor.RPM
-        print "Sensor {}: {} {}".format(sensor.title, sensor.value, sensor.unit)
+
+# Retrieves value from one sensor
+with scanner.OBDScanner("/dev/pts/5") as scan:
+    while True:
+        # Engine coolant temperature
+        sensor = scan.sensor[01](05)
+        print("Sensor {}: {} {}".format(sensor.title, sensor.value, sensor.unit))
+        time.sleep(0.5)

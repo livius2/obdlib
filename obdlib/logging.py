cc2bfd688a036117e86dcc7ced3e31b67c119af9
Siarhei Boika <s.s.boika@gmail.com>
2015-07-05 20:28:23 +0300

    Added special log methods to Logging, added unittest

--- obdlib/logging.py
+++ obdlib/logging.py
@@ -27,20 +27,20 @@ def asctime(t = None):
 
 class Logging():
 
-    entry_format = "{time}{pfx}\n"
+    entry_format = "{time} : {pfx} : {log_lvl} : {msg}\n"
 
-    prefix = 'OBDLIB:'
+    prefix = 'OBDLIB'
 
-    __logging_levels = [
-    'CRITICAL',
-    'ERROR',
-    'WARNING',
-    'INFO',
-    'DEBUG',
-    'NOTSET',
-    ]
+    __logging_levels = (
+    'CRITICAL', #0
+    'ERROR',    #1
+    'WARNING',  #2
+    'INFO',     #3
+    'DEBUG',    #4
+    'NOTSET',   #5,-1
+    )
 
-    def __logtime(t = None):
+    def __logtime(self, t = None):
         """
         Converts the 8-tuple which contains:
         (year, month, mday, hour, minute, second, weekday, yearday)
@@ -48,20 +48,48 @@ class Logging():
         '1981-05-31 01:03:59'
         """
 
-        t = t or time.lcaltime())
-        result = "{0:04}-{1:02}-{2:02} {3:02}:{4:02}:{5:02}:".format(*t)
+        t = t or time.localtime()
+        result = "{0:04}-{1:02}-{2:02} {3:02}:{4:02}:{5:02}".format(*t)
         return result
 
-    def __init__(log_level = 1, duplicate_in_stdout = False, output = None):
+    def get_log_level(self, log_level):
+        if log_level > len(self.__logging_levels)-1:
+            log_level = -1
+        return self.__logging_levels[log_level]
+
+    def __init__(self, log_level=1, duplicate_in_stdout=False, output=None):
         self.use_stdout = duplicate_in_stdout
         self.output_stream = output
         self.log_level = log_level
 
-    def __call__(self, level, msg, force = False):
-
-        out_msg = .format(prefix, str(msg), )
+    def __call__(self, msg, level = 5, force = False):
+        if level > self.log_level:
+            return
+        out_msg = self.entry_format.format(time = self.__logtime(),
+                                           pfx = self.prefix,
+                                           log_lvl = self.get_log_level(level),
+                                           msg = str(msg))
 
         if self.use_stdout or force:
             stdout.write(out_msg)
+
+    def critical(self, msg):
+        return self(msg, 0)
+
+    def error(self, msg):
+        return self(msg, 1)
+
+    def warning(self, msg):
+        return self(msg, 2)
+
+    def info(self, msg):
+        return self(msg, 3)
+
+    def debug(self, msg):
+        return self(msg, 4)
+
+    def log(self, msg):
+        return self(msg, 5)
+
 
 logger = Logging()

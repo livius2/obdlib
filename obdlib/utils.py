51edff298a79e1791e2c7a75c4683a10314a466c
Yury Kavaliou <developer@EPBYMINW4779.minsk.epam.com>
2015-07-07 10:20:56 +0300

    Fixed: scanner.initialize: moved get proto name after 01 PID 00 commabd, zfill: changed int(...) to __digit(...)

--- obdlib/utils.py
+++ obdlib/utils.py
@@ -24,7 +24,7 @@ def zfill(string, width):
         'zfill') else (
         '{0:0%d}' %
         (width)).format(
-            int(string))
+            __digit(string))
 
 
 def bitwise_pids(hex_string, start=0):

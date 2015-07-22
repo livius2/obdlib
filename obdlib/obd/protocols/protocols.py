254906d04ffddb49d30f7e5f771023ffee86bf3b
Yury Kavaliou <developer@EPBYMINW4779.minsk.epam.com>
2015-07-22 16:36:49 +0300

    Improved method create_data

--- obdlib/obd/protocols/protocols.py
+++ obdlib/obd/protocols/protocols.py
@@ -22,53 +22,50 @@ class Protocols(Base):
         if raw_data:
             ecu_messages = self.remove_searching(raw_data)
 
-            if self.check_result(
-                    ecu_messages) and self.check_error(ecu_messages):
+            if self.check_message(ecu_messages):
                 if self.header:
-                    if len(ecu_messages):
-                        self.check_sum = -2
-                        # sorts ECU's messages
-                        ecu_messages = sorted(ecu_messages)
-                        service_data = self._parse_headers(ecu_messages)
-                        for message in ecu_messages:
-                            # if one ECU returns multi line
-                            # multi line includes line number byte
-                            ecu_number, response_mode = self._get_frame_params(message)
+                    self.check_sum = -2
+                    # sorts ECU's messages
+                    ecu_messages = sorted(ecu_messages)
+                    service_data = self._parse_headers(ecu_messages)
+                    for message in ecu_messages:
+                        # if one ECU returns multi line
+                        # multi line includes line number byte
+                        ecu_number, response_mode = self._get_frame_params(message)
 
-                            # check if response trouble codes
-                            if response_mode == 43:
-                                # add fake byte after the mode one
-                                message = self._add_additional_byte(message)
-                                self.check_sum = None
+                        # check if response trouble codes
+                        if response_mode == 43:
+                            # add fake byte after the mode one
+                            message = self._add_additional_byte(message)
+                            self.check_sum = None
 
-                            if service_data[ecu_number] > 1:
-                                # multi line response - ELM spec page 42
-                                # response format priority:receiver:transmitter:mode:pid:line_number:data:checksum
-                                # >0902 - (ex: get VIN)
-                                # 86 F1 10 49 02 01 00 00 00 31 FC
-                                # 86 F1 10 49 02 02 44 34 47 50 FC
-                                # 86 F1 10 49 02 03 30 30 52 35 FC
-                                # 86 F1 10 49 02 04 35 42 31 32 FC
-                                # 86 F1 10 49 02 05 33 34 35 36 FC
-                                # 6 * 2: means that we are removed
-                                # "mode:pid:line" info from the record
-                                try:
-                                    data[ecu_number] += self._get_multi_data(message)
-                                except KeyError:
-                                    data[ecu_number] = self._get_multi_data(message)
-                            # frame without line number byte
-                            # removes header and checksum
-                            # format priority:receiver:transmitter:mode:pid:data:checksum
-                            # [ header][serv][   data   ][CS]
-                            # 86 F1 10 41 00 FF FF FF FF FC  - ELM spec page 38
-                            else:
-                                data[ecu_number] = self.get_data(message)
-                    else:
-                        mess = "Error response data"
-                        logger.error(mess)
-                        raise Exception(mess)
+                        self._process(data, message, ecu_number, service_data[ecu_number])
 
         return data
+
+    def _process(self, data, message, ecu_number, multi):
+        if multi > 1:
+            # multi line response - ELM spec page 42
+            # response format priority:receiver:transmitter:mode:pid:line_number:data:checksum
+            # >0902 - (ex: get VIN)
+            # 86 F1 10 49 02 01 00 00 00 31 FC
+            # 86 F1 10 49 02 02 44 34 47 50 FC
+            # 86 F1 10 49 02 03 30 30 52 35 FC
+            # 86 F1 10 49 02 04 35 42 31 32 FC
+            # 86 F1 10 49 02 05 33 34 35 36 FC
+            # 6 * 2: means that we are removed
+            # "mode:pid:line" info from the record
+            try:
+                data[ecu_number] += self._get_multi_data(message)
+            except KeyError:
+                data[ecu_number] = self._get_multi_data(message)
+        # frame without line number byte
+        # removes header and checksum
+        # format priority:receiver:transmitter:mode:pid:data:checksum
+        # [ header][serv][   data   ][CS]
+        # 86 F1 10 41 00 FF FF FF FF FC  - ELM spec page 38
+        else:
+            data[ecu_number] = self.get_data(message)
 
     def _get_multi_data(self, record):
         """
